cbe61c9fcac7f7a780b1ccd8c8ad3a52bc73c8c7
lolko <alfavanish@mail.ru>
2022-06-16 16:12:58 +0300

    task 6,8

--- task005.java
+++ task005.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 public class task005 {
---
+++ task008.java
@@ -0,0 +1,54 @@
+
+public class task008 {
+
+    static int[][] board;
+    static int[][] moves;
+    static int maxMoves;
+
+    public static void main(String[] args) {
+        board = new int[5][5];
+        maxMoves = board.length * board[0].length;
+        moves = new int[][] { { -2, 1 }, { -2, -1 },
+                { -1, 2 }, { 1, 2 },
+                { 2, 1 }, { 2, -1 },
+                { -1, -2 }, { -1, -2 } };
+
+        for (int i = 0; i < board.length; i ++){
+            for (int j = 0; j < board[0].length; j++){
+                if(findPath(i, j, 1)){
+                    printSol();
+                    return;
+                }
+            }
+        }
+        System.out.println("Нет решений");
+    }
+
+    static boolean isPossible(int x, int y){
+        return x >=0 && x < board.length && y >= 0 && y < board.length && board[x][y] == 0; 
+    }
+
+    static boolean findPath(int curX, int curY, int moveNum){
+        board[curX][curY] = moveNum;
+        if(moveNum >= maxMoves) return true;
+        
+        for(int i = 0; i < 8; i++){
+            int nextX = curX + moves[i][0];
+            int nextY = curY + moves[i][1];
+            if(isPossible(nextX, nextY) && findPath(nextX, nextY, moveNum + 1)){
+                return true;
+            }
+        }
+        board[curX][curY] = 0;
+        return false;
+    }
+
+    static void printSol(){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                System.out.println(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
